c9da077faeaa6413fcb7ddc02b9b645bd2bbffe9
JMignone <6mignone@gmail.com>
2021-03-31 19:33:19 -0400

    Initial Commit
    by Milk

---
+++ New Unity Project/Assets/Scenes/_Scripts/Units/Unit.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField]
+    private Actor3D agent;
+
+    [SerializeField]
+    private Actor2D unitSprite;
+
+    [SerializeField]
+    private GameObject target;
+
+    public Actor3D Agent
+    {
+        get { return agent; }
+        //set { agent = value; }
+    }
+
+    public Actor2D UnitSprite
+    {
+        get { return unitSprite; }
+        //set { agent = value; }
+    }
+
+    public GameObject Target
+    {
+        get { return target; }
+        set { target = value; }
+    }
+
+    private void Update()
+    {
+        if(target != null)
+            agent.Agent.SetDestination(target.transform.position);
+    }
+
+}
