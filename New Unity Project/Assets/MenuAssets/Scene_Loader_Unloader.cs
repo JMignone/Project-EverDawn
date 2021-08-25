c4a351bad56a9322a04e5b3c3b69c171d62fd4d8
0Tateki0 <alex.futa@gmail.com>
2021-08-25 17:44:41 -0600

    Luna_UI_10
    Added `LoadSceneAdditive()` to `Scene_Loader_Unloader` script so other scenes can be loaded without unloading all other scenes.
    
    Changed Test game button to use `LoadSceneAdditive()`

--- New Unity Project/Assets/MenuAssets/Scene_Loader_Unloader.cs
+++ New Unity Project/Assets/MenuAssets/Scene_Loader_Unloader.cs
@@ -5,15 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Loader_Unloader : MonoBehaviour
 {
-   //Recieve a scene's name as a string to load
-   public void LoadScene(string SceneName)
-   {
-        //Load the given scene
+    //Recieve a scene's name as a string to load
+    public void LoadScene(string SceneName)
+    {
+        //Load the given scene and unload all other scenes
         SceneManager.LoadSceneAsync(SceneName);
-   }
+    }
 
-   //Recieve a scene's name as a string to unload
-   public void UnloadScene(string SceneName)
+    //Recieve a scene's name as a string to load additively
+    public void LoadSceneAdditive(string SceneName)
+    {
+        //Load the given scene without unloading any other scenes
+        SceneManager.LoadSceneAsync(SceneName, LoadSceneMode.Additive);
+    }
+
+    //Recieve a scene's name as a string to unload
+    public void UnloadScene(string SceneName)
     {
         //Unload the given scene
         SceneManager.UnloadSceneAsync(SceneName);
