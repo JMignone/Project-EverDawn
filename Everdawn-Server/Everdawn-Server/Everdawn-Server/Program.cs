96ff2daf6a061b61c3d621fc80c6dd73c8556606
Kuma <20806030+lkkuma@users.noreply.github.com>
2021-09-04 21:35:28 -0400

    InDev 0, Version 0, Build 1 (v0.0.1)
    First server build with basic structure to read and accept incoming connections

---
+++ Everdawn-Server/Everdawn-Server/Everdawn-Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Everdawn_Server
+{
+    class Program
+    {
+        private static readonly string __VERSION = "0.0.1";
+        private static readonly string ServerTitle = "Everdawn Server v" + __VERSION;
+        private static readonly int __PORT = 49368;
+
+        static void Main(string[] args)
+        {
+            Console.Title = ServerTitle;
+
+            Server.Start(10, __PORT);
+
+            Console.ReadKey();
+        }
+    }
+}
