6cfb939a769c1119d46ee2e411e8537688ac5da9
JMignone <6mignone@gmail.com>
2022-02-16 20:22:04 -0500

    multi deck and bot deck added
    5 deck slots added
    
    5th slot is reserved for bot

--- Project Everdawn/Assets/MenuAssets/DeckSaving/DeckDisplay.cs
+++ Project Everdawn/Assets/MenuAssets/DeckSaving/DeckDisplay.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class DeckDisplay : MonoBehaviour
 {
-    [SerializeField] private SO_CardDatabase cardDatabase;
+    [SerializeField] private DeckManager deckManager;
     [SerializeField] private GameObject cardPrefab;
-    [SerializeField] private DeckSaver deckSaver;
     [SerializeField] private bool saveDeckOnUnload;
+    [SerializeField] private bool allowDeckMultiselection;
 
     [Space]
     
-    [SerializeField] private List<int> cardIDList = new List<int>();
+    [SerializeField] [Min(1)] private int selectedDeckNumber;
+    [SerializeField] private PlayerDeck selectedDeck = new PlayerDeck();
     public List<GameObject> cardsBeingDisplayed = new List<GameObject>();
 
     private GameObject cardInstance;
@@ -19,7 +20,7 @@ public class DeckDisplay : MonoBehaviour
 
     private void OnEnable()
     {
-        LoadDeck();
+        LoadDeck(selectedDeckNumber);
     }
 
     private void OnDisable()
@@ -27,39 +28,46 @@ public class DeckDisplay : MonoBehaviour
         UnloadDeck();
     }
 
-    private void LoadDeckData()
-    {
-        deckSaver.Load();
-        cardIDList = deckSaver.selectedCards;
-    }
-
-    private void SaveDeckData()
+    private void SaveDeck()
     {
         for(int i = 0; i < cardsBeingDisplayed.Count; i++) //Loop over the new list of cards and set the local list of IDs to reflect changes
         {
-            cardIDList[i] = cardsBeingDisplayed[i].GetComponent<CardDisplay>().cardID;
+            selectedDeck.cardsInDeck[i] = cardsBeingDisplayed[i].GetComponent<CardDisplay>().cardID;
         }
-        deckSaver.selectedCards = cardIDList;
-        deckSaver.Save();
+        deckManager.selectedDeck = selectedDeck;
+        deckManager.SaveSelectedDeck(selectedDeckNumber);
     }
 
-    private void LoadDeck()
+    public void LoadDeck(int deckNumber)
     {
-        LoadDeckData();
-        for (int i = 0; i < cardIDList.Count; i++)
+        selectedDeckNumber = deckNumber;
+        if(deckNumber != 0)
+            deckManager.selectedDeckNumber = deckNumber;
+            
+        deckManager.LoadDeck(deckNumber);
+        selectedDeck = deckManager.selectedDeck;
+        List<SO_Card> cardList = new List<SO_Card>();
+        cardList = deckManager.ConvertIntListToCardList(selectedDeck.cardsInDeck);
+        for (int i = 0; i < selectedDeck.cardsInDeck.Count; i++)
         {
             CardDisplay cardToDisplay = cardPrefab.GetComponent<CardDisplay>(); // Pull card data from card database
-            cardToDisplay.card = cardDatabase.cardList[cardIDList[i]]; // Set new card's instance data to be the data pulled from the database
+            cardToDisplay.card = cardList[i]; // Set new card's instance data to be the data pulled from the database
             cardInstance = Instantiate(cardToDisplay.gameObject, this.transform); // Instantiate the card from the list
             cardsBeingDisplayed.Add(cardInstance); // Add the new card instance to the list of cards
         }
+    }
+
+    public void ChangeDeck(int deckNumber)
+    {
+        UnloadDeck();
+        LoadDeck(deckNumber);
     }
 
     private void UnloadDeck()
     {
         if(saveDeckOnUnload == true)
         {
-        SaveDeckData();
+        SaveDeck();
         }
 
         if(cardsBeingDisplayed != null && transform.childCount != 0) // Check that there are objects that have been instantiated and are in the list
@@ -82,7 +90,10 @@ public class DeckDisplay : MonoBehaviour
         if(selectedCardDisplay != null)
         {
             cd.BindCardData(selectedCardDisplay.card);
-            selectedCardDisplay = null; // Prevents only tapping bottom card once and being able to change all top cards without re-inputting any other actions - could be useful for testing down the line? add toggle?
+            if(allowDeckMultiselection == false)
+            {
+                selectedCardDisplay = null; // Prevents only tapping bottom card once and being able to change all top cards without re-inputting any other actions - could be useful for testing down the line? add toggle?
+            }
         }
     }
 }
