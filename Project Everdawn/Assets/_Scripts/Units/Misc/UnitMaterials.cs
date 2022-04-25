7896a0169b643cc4e24a55aab694d3a51e71aade
EagleFG <wesley.n.campbell@gmail.com>
2022-04-25 02:20:13 -0700

    Material swapping implemented
    Still need to do visuals for spells - need Milk's help for that

--- Project Everdawn/Assets/_Scripts/Units/Misc/UnitMaterials.cs
+++ Project Everdawn/Assets/_Scripts/Units/Misc/UnitMaterials.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 [System.Serializable]
 public class UnitMaterials
 {
+    /*
     [SerializeField]
     private List<Renderer> renderers;
+    */
 
     //[SerializeField]
     //private bool transparent;
 
-    private Color emissionColor; //emission color is the what makes a unit appear frozen, poisoned, or hovered by an ability
+    //private Color emissionColor; //emission color is the what makes a unit appear frozen, poisoned, or hovered by an ability
+
+    [SerializeField]
+    private StatusEffectMaterialSwapping materialSwapper;
+
     private bool hovered;
 
 /*
@@ -20,7 +26,7 @@ public class UnitMaterials
         get { return transparent; }
         set { transparent = value; }
     }
-*/
+
 
     public List<Renderer> Renderers
     {
@@ -30,8 +36,10 @@ public class UnitMaterials
     public void Start() {
         emissionColor = new Color(1f,1f,1f,1f);
     }
-
-    public void MakeInvisible() {
+*/
+    public void MakeInvisible()
+    {
+        /*
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             mat.SetColor("_Color", new Color(1f,1f,1f,0));
@@ -42,9 +50,14 @@ public class UnitMaterials
             mat.renderQueue = 3000;
         }
         //transparent = true;
+        */
+
+
     }
 
-    public void MakeTransparent() {
+    public void MakeTransparent()
+    {
+        /*
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             mat.SetColor("_Color", new Color(1f,1f,1f,.65f));
@@ -55,9 +68,14 @@ public class UnitMaterials
             mat.renderQueue = 3000;
         }
         //transparent = true;
+        */
+
+
     }
 
-    public void MakeOpaque() {
+    public void MakeOpaque()
+    {
+        /*
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             mat.SetColor("_Color", new Color(1f,1f,1f,1f));
@@ -68,45 +86,86 @@ public class UnitMaterials
             mat.renderQueue = -1;
         }
         //transparent = false;
+        */
+
+
     }
 
-    public void TintPurple() {
+    public void TintPurple()
+    {
+        /*
         emissionColor = new Color(emissionColor.r + .1569f, emissionColor.g, emissionColor.b + .4706f, 0);
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             if(!hovered)
                 mat.SetColor("_EmissionColor", emissionColor);
         }
+        */
+
+        if (hovered == false)
+        {
+            Vector4 statusMaterialHDRColor = materialSwapper.StatusMaterialHDRColor;
+            materialSwapper.ChangeHDRColor(new Vector4(statusMaterialHDRColor.x + .1569f, statusMaterialHDRColor.y, statusMaterialHDRColor.z + .4706f, 1));
+        }
     }
 
-    public void RemovePurple() {
+    public void RemovePurple()
+    {
+        /*
         emissionColor = new Color(emissionColor.r - .1569f, emissionColor.g, emissionColor.b - .4706f, 0);
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             if(!hovered)
                 mat.SetColor("_EmissionColor", emissionColor);
         }
+        */
+
+        if (hovered == false)
+        {
+            Vector4 statusMaterialHDRColor = materialSwapper.StatusMaterialHDRColor;
+            materialSwapper.ChangeHDRColor(new Vector4(statusMaterialHDRColor.x - .1569f, statusMaterialHDRColor.y, statusMaterialHDRColor.z - .4706f, 1));
+        }
     }
 
-    public void TintCyan() {
+    public void TintCyan()
+    {
+        /*
         emissionColor = new Color(emissionColor.r + .0784f, emissionColor.g + .451f, emissionColor.b + .39216f, 0);
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             if(!hovered)
                 mat.SetColor("_EmissionColor", emissionColor);
         }
+        */
+
+        if (hovered == false)
+        {
+            Vector4 statusMaterialHDRColor = materialSwapper.StatusMaterialHDRColor;
+            materialSwapper.ChangeHDRColor(new Vector4(statusMaterialHDRColor.x + .0784f, statusMaterialHDRColor.y + .451f, statusMaterialHDRColor.z + .39216f, 1));
+        }
     }
 
-    public void RemoveCyan() {
+    public void RemoveCyan()
+    {
+        /*
         emissionColor = new Color(emissionColor.r - .0784f, emissionColor.g - .451f, emissionColor.b - .39216f, 0);
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             if(!hovered)
                 mat.SetColor("_EmissionColor", emissionColor);
         }
+        */
+
+        if (hovered == false)
+        {
+            Vector4 statusMaterialHDRColor = materialSwapper.StatusMaterialHDRColor;
+            materialSwapper.ChangeHDRColor(new Vector4(statusMaterialHDRColor.x - .0784f, statusMaterialHDRColor.y - .451f, statusMaterialHDRColor.z - .39216f, 1));
+        }
     }
 
-    public void AbilityHover() {
+    public void AbilityHover()
+    {
+        /*
         Color color = new Color(.5f, 0, 0, 1f);
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
@@ -114,16 +173,28 @@ public class UnitMaterials
             //mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.None;
             //mat.EnableKeyword("_EMISSION");
         }
+        */
+
+        Vector4 statusMaterialHDRColor = materialSwapper.StatusMaterialHDRColor;
+        materialSwapper.ChangeHDRColor(new Vector4(.5f, 0, 0, 1));
+
         hovered = true;
     }
 
-    public void RemoveAbilityHover() {
+    public void RemoveAbilityHover()
+    {
+        /*
         foreach(Renderer renderer in renderers) {
             Material mat = renderer.material;
             mat.SetColor("_EmissionColor", emissionColor);
             //mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.None;
             //mat.DisableKeyword("_EMISSION");
         }
+        */
+
+        Vector4 statusMaterialHDRColor = materialSwapper.StatusMaterialHDRColor;
+        materialSwapper.ChangeHDRColor(new Vector4(0, 0, 0, 1));
+
         hovered = false;
     }
 }
